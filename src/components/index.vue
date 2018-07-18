c23066517ee4cdf62637077d23976475b62f7415
tanaka <himilk6@yahoo.co.jp>
2018-07-18 11:57:11 +0900

    file-loader comp

--- src/components/index.vue
+++ src/components/index.vue
@@ -47,8 +47,12 @@
 
 <script>
 import moment from 'moment'
+// Webpack CSS import
+import 'onsenui/css/onsenui.css'
+import 'onsenui/css/onsen-css-components.css'
+// JS import
 import Vue from 'vue'
-import VueOnsen from 'vue-onsenui' // This already imports 'onsenui'
+import VueOnsen from 'vue-onsenui'
 Vue.use(VueOnsen);
 
 
